8fc711b726897b41a0e5376fb4ef818aa2be0338
Pete Low <pedritolow@gmail.com>
2021-07-26 14:54:47 -0700

    LocationSelect Screen update

--- src/screens/LocationSelectScreen.js
+++ src/screens/LocationSelectScreen.js
@@ -1,26 +1,75 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import './LocationScreen.css'
 
 const LocationSelectScreen = () => {
     return (
         <div>
-           <h1>Location Select Screen</h1>
-           
-           <section>
-            <div class="tab-row">
-            <div class="col-4">
-                <h2>Columns</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga porro sapiente, corporis dignissimos laudantium illo expedita corrupti asperiores esse quae.</p>
-            </div>
-            <div class="col-4">
-                <h2>Columns</h2>
-                <p>Repellat qui, dolores nemo asperiores incidunt quidem dolorum. Quos, deleniti neque architecto dolore magni aliquid. Inventore suscipit consequuntur excepturi ipsam.</p>
-            </div>
-            <div class="col-4">
-                <h2>Columns</h2>
-                <p>Doloribus perferendis a ipsum ad, consectetur animi aliquam maxime, earum aspernatur, ea commodi tempore suscipit explicabo cupiditate at nemo quae.</p>
-            </div>
-            </div>
-        </section>
+            <h1>Whats the point of going to:</h1>
+           <Row>
+               <Col sm={12} md={6} lg={4} xl={3}>
+            <a className='' href='/pnp'>
+                <div className='destinationChoice1'>
+                    <div className='title-wrapper'>
+                        <h3>Point No Point</h3>
+                        <span></span>
+                    </div>
+                </div>
+            </a>
+            </Col>
+            <Col sm={12} md={6} lg={4} xl={3}>
+            <a className='' href='portGamble'>
+                <div className='destinationChoice2'>
+                    <div className='title-wrapper'>
+                        <h3>Port Gamble</h3>
+                        <span></span>
+                    </div>
+                </div>
+            </a>
+            </Col>
+            <Col sm={12} md={6} lg={4} xl={3}>
+            <a className='' href='orcasIsland'>
+                <div className='destinationChoice3'>
+                    <div className='title-wrapper'>
+                        <h3>Orcas Island</h3>
+                        <span></span>
+                    </div>
+                </div>
+            </a>
+            </Col>
+            </Row>
+            <Row>
+               <Col sm={12} md={6} lg={4} xl={3}>
+            <a className='' href='paulsboro'>
+                <div className='destinationChoice4'>
+                    <div className='title-wrapper'>
+                        <h3>Paulsboro</h3>
+                        <span></span>
+                    </div>
+                </div>
+            </a>
+            </Col>
+            <Col sm={12} md={6} lg={4} xl={3}>
+            <a className='' href='kingston'>
+                <div className='destinationChoice5'>
+                    <div className='title-wrapper'>
+                        <h3>Kingston</h3>
+                        <span></span>
+                    </div>
+                </div>
+            </a>
+            </Col>
+            <Col sm={12} md={6} lg={4} xl={3}>
+            <a className='' href='camanoIsland'>
+                <div className='destinationChoice6'>
+                    <div className='title-wrapper'>
+                        <h3>Camano Island</h3>
+                        <span></span>
+                    </div>
+                </div>
+            </a>
+            </Col>
+            </Row>
         </div>
     )
 }
