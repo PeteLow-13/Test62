65ea7a4f7929a2a5f900d9edd38e87bf6d720e02
Pete Low <pedritolow@gmail.com>
2021-08-04 13:43:16 -0700

    added store.js

---
+++ frontend/store.js
@@ -0,0 +1,34 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+// import {} from './reducers/locationReducers';
+import { cartReducer } from './reducers/cartReducers';
+// import {
+//   userLoginReducer,
+//   userRegisterReducer,
+//   userDetailsReducer,
+//   userUpdateProfileReducer,
+//   userListReducer,
+//   userDeleteReducer,
+//   userUpdateReducer,
+// } from './reducers/userReducers';
+
+const reducer = combineReducers({});
+
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
