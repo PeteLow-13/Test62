da6ddb2072fc3e86fb0dee368c1945b9ed9f5cb8
Pete Low <pedritolow@gmail.com>
2021-08-06 16:45:30 -0700

    listLocationDetails to location actions

--- frontend/src/actions/locationActions.js
+++ frontend/src/actions/locationActions.js
@@ -3,6 +3,9 @@ import {
   LOCATION_LIST_REQUEST,
   LOCATION_LIST_SUCCESS,
   LOCATION_LIST_FAIL,
+  LOCATION_DETAILS_REQUEST,
+  LOCATION_DETAILS_SUCCESS,
+  LOCATION_DETAILS_FAIL,
 } from '../constants/locationConstants';
 
 export const listLocations =
@@ -29,3 +32,24 @@ export const listLocations =
       });
     }
   };
+
+export const listLocationDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: LOCATION_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/locations/${id}`);
+
+    dispatch({
+      type: LOCATION_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOCATION_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
