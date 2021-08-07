2aa3c176477bdc0d94dee1311c9b1c3a5e61946a
Pete Low <pedritolow@gmail.com>
2021-08-07 12:58:25 -0700

    Home screen changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,24 +1,43 @@
-import { coverage } from 'browserslist'
-import React from 'react'
-import { Container, Image, Col, Row,} from 'react-bootstrap'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Col, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Location from '../Components/Location';
+import Loader from '../Components/Loader';
+import Message from '../Components/Message';
 
+import { listLocations } from '../actions/locationActions';
 
-const HomeScreen = () => {
-    return (
-        < >
-            <div className='home-container'>
-            <h1>The Point of Going</h1>
-            </div>
-            <div className='second-section'>
-                <h2>Lorem</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, mollitia atque fugit pariatur natus praesentium fuga, cumque commodi dolorum rerum sunt vitae voluptatum placeat consectetur dolores, dolore maxime voluptates quia.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, mollitia atque fugit pariatur natus praesentium fuga, cumque commodi dolorum rerum sunt vitae voluptatum placeat consectetur dolores, dolore maxime voluptates quia.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, mollitia atque fugit pariatur natus praesentium fuga, cumque commodi dolorum rerum sunt vitae voluptatum placeat consectetur dolores, dolore maxime voluptates quia.</p>
-            </div>
-            
-        
-        </>
-    )
-}
+const HomeScreen = ({}) => {
+  const locationList = useSelector((state) => state.locationList);
 
-export default HomeScreen
+  const { loading, error, locations } = locationList;
+  // add page and pages where pagination is needed
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(listLocations);
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>Locations</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {locations.map((location) => (
+            <Col sm={12} md={12} lg={6} xl={6}>
+              <Location location={location} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
