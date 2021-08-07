a1294c746b2950d21d3f9500c305e3fdf8f26f58
Pete Low <pedritolow@gmail.com>
2021-08-06 18:47:07 -0700

    delete create reducers

--- frontend/src/reducers/locationReducers.js
+++ frontend/src/reducers/locationReducers.js
@@ -2,6 +2,15 @@ import {
   LOCATION_LIST_REQUEST,
   LOCATION_LIST_SUCCESS,
   LOCATION_LIST_FAIL,
+  LOCATION_DETAILS_REQUEST,
+  LOCATION_DETAILS_SUCCESS,
+  LOCATION_DETAILS_FAIL,
+  LOCATION_DELETE_FAIL,
+  LOCATION_DELETE_SUCCESS,
+  LOCATION_DELETE_REQUEST,
+  LOCATION_CREATE_REQUEST,
+  LOCATION_CREATE_SUCCESS,
+  LOCATION_CREATE_FAIL,
 } from '../constants/locationConstants';
 
 export const locationListReducer = (state = { locations: [] }, action) => {
@@ -33,6 +42,34 @@ export const locationDetailsReducer = (state = { location: {} }, action) => {
       return { loading: false, location: action.payload };
     case LOCATION_DETAILS_FAIL:
       return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const locationDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case LOCATION_DELETE_REQUEST:
+      return { loading: true, ...state };
+    case LOCATION_DELETE_SUCCESS:
+      return { loading: false, success: true };
+    case LOCATION_DELETE_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const locationCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case LOCATION_CREATE_REQUEST:
+      return { loading: true };
+    case LOCATION_CREATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload };
+    case LOCATION_CREATE_FAIL:
+      return { loading: false, error: action.payload };
+    case LOCATION_CREATE_RESET:
+      return {};
     default:
       return state;
   }
