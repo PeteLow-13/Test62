6433ff98421b4dc455e62978332418e75d0924e4
Pete Low <pedritolow@gmail.com>
2021-08-06 18:43:48 -0700

    delete and create actions to productActions

--- frontend/src/actions/locationActions.js
+++ frontend/src/actions/locationActions.js
@@ -6,6 +6,12 @@ import {
   LOCATION_DETAILS_REQUEST,
   LOCATION_DETAILS_SUCCESS,
   LOCATION_DETAILS_FAIL,
+  LOCATION_DELETE_FAIL,
+  LOCATION_DELETE_SUCCESS,
+  LOCATION_DELETE_REQUEST,
+  LOCATION_CREATE_REQUEST,
+  LOCATION_CREATE_SUCCESS,
+  LOCATION_CREATE_FAIL,
 } from '../constants/locationConstants';
 
 export const listLocations =
@@ -50,6 +56,77 @@ export const listLocationDetails = (id) => async (dispatch) => {
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
+    });
+  }
+};
+
+export const deleteLocation = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: LOCATION_DELETE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    await axios.delete(`/api/locations/${id}`, config);
+
+    dispatch({
+      type: LOCATION_DELETE_SUCCESS,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === 'Not Authorized, token failed') {
+    }
+    dispatch({
+      type: LOCATION_DELETE_FAIL,
+      payload: message,
+    });
+  }
+};
+
+export const createLocation = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: LOCATION_CREATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.post(`/api/locations`, {}, config);
+
+    dispatch({
+      type: LOCATION_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === 'Not Authorized, token failed') {
+    }
+    dispatch({
+      type: LOCATION_CREATE_FAIL,
+      payload: message,
     });
   }
 };
