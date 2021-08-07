98f5b59b68b4de3540eada957d93081d5d28d699
Pete Low <pedritolow@gmail.com>
2021-08-07 12:58:43 -0700

    LocationScreen

---
+++ frontend/src/screens/LocationScreen.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Form,
+} from 'react-bootstrap';
+import Message from '../Components/Message';
+import Loader from '../Components/Loader';
+import { listLocationDetails } from '../actions/locationActions';
+
+const LocationScreen = () => {
+  const locationDetails = useSelector((state) => state.locationDetails);
+  const { loading, error, location } = locationDetails;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  return (
+    <>
+      <Link className='btn btn-light my-3' to='/locationSelectScreen'>
+        Back to Locations
+      </Link>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <>
+          <h1>What's the point of going to {location.name}?</h1>
+          <Row>
+            <Col md={12}>
+              <Image src={location.image} alt={location.name} fluid />
+            </Col>
+          </Row>
+        </>
+      )}
+    </>
+  );
+};
+
+export default LocationScreen;
