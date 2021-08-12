baf4d685bc56bf36e0eed7730d82c97b3ac72465
Pete Low <pedritolow@gmail.com>
2021-08-11 21:08:45 -0700

    fiddling with burger bar

--- frontend/src/Components/BurgerButton.js
+++ frontend/src/Components/BurgerButton.js
@@ -1,19 +1,37 @@
 import React from 'react';
 
 const BurgerButton = () => {
-  return <div></div>;
+  const menuBtn = document.querySelector('.menu-btn');
+  let menuOpen = false;
+
+  menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+      menuBtn.classList.add('open');
+      menuOpen = true;
+    } else {
+      menuBtn.classList.remove('open');
+      menuOpen = false;
+    }
+  });
+  return (
+    <>
+      <div className='menu-btn'>
+        <div className='menu-btn_burger'></div>
+      </div>
+    </>
+  );
 };
 
 export default BurgerButton;
 
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if (!menuOpen) {
-    menuBtn.classList.add('open');
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove('open');
-    menuOpen = false;
-  }
-});
+// const menuBtn = document.querySelector('.menu-btn');
+// let menuOpen = false;
+// menuBtn.addEventListener('click', () => {
+//   if (!menuOpen) {
+//     menuBtn.classList.add('open');
+//     menuOpen = true;
+//   } else {
+//     menuBtn.classList.remove('open');
+//     menuOpen = false;
+//   }
+// });
