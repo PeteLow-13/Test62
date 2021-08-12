0df481fc42fcbe8e1bdbaf3458c456f23ea4e66b
Pete Low <pedritolow@gmail.com>
2021-08-11 20:16:31 -0700

    add Play.js

---
+++ frontend/src/Components/Play.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import TagBar from './TagBar';
+
+const Play = () => {
+  const locationDetails = useSelector((state) => state.locationDetails);
+  const { location } = locationDetails;
+
+  return (
+    <>
+      <Row className='play' id='play'>
+        <h2>where to Play {location.name}</h2>
+        <Col sm={12} md={4} lg={4} xl={4}>
+          <Card>
+            <Card.Img src={location.image} />
+            <Card.Body>
+              <Card.Title>Scratch kitchen</Card.Title>
+              <Card.Text className='address'>
+                32420 N Rainier Ave ​ Port Gamble, WA 98364
+              </Card.Text>
+              <Card.Text className='phone'>(360) 297-7636</Card.Text>
+              <Card.Text className='description'>
+                Good food, cold beer, nice people, awesome view. Local and
+                organic ingredients, offering compostable to go products too!
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={12} md={4} lg={4} xl={4}>
+          <Card>
+            <Card.Img src={location.image} />
+            <Card.Body>
+              <Card.Title>Port Gamble General Store</Card.Title>
+              <Card.Text className='address'>
+                32400 N Rainier Ave ​Port Gamble, WA 98364{' '}
+              </Card.Text>
+              <Card.Text className='phone'>(360) 297-7636</Card.Text>
+              <Card.Text className='description'>
+                A full cafe menu including sandwiches, soups, fresh baked goods,
+                ice cream, snacks, an espresso bar, and retail space with gifts.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={12} md={4} lg={4} xl={4}>
+          <Card>
+            <Card.Img src={location.image} />
+            <Card.Body>
+              <Card.Title>Butcher & Baker</Card.Title>
+              <Card.Text className='address'>
+                4719 NE State Highway 104, Port Gamble WA 98364
+              </Card.Text>
+              <Card.Text className='phone'>(360) 297-9500</Card.Text>
+              <Card.Text className='description'>
+                A locally sourced farmhouse restaurant, butcher shop and bakery
+                that offers small batch provisions and dining.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <TagBar />
+      </Row>
+    </>
+  );
+};
+
+export default Play;
