a1a74b573489210656e6824a47986b6ed03dc502
Pete Low <pedritolow@gmail.com>
2021-08-19 16:00:33 -0700

    Port gamle points added

--- backend/data/locations.js
+++ backend/data/locations.js
@@ -2,7 +2,14 @@ const locations = [
   {
     name: 'Port Gamble',
     image: '/images/PortGambleMainFull.jpg',
-    point: '',
+    point1:
+      'Visiting Port Gamble is like a trip to the 19th Century. It was founded in 1853, by A.J. Pope, Captain William Talbot, and Cyrus Walker, and designed to look like their home of East Machias, Maine. The town is immaculately maintained, and a U.S. National Historic Landmark. The National Park Service called Port Gamble "the finest example of a nineteenth century Pacific Coast logging community.” Many of the historic homes are now shops.',
+    point2:
+      'The general store was the first building constructed, and has been in operation since 1853. The store is a charming tourist attraction, plus a great spot for food, supplies, and gifts. It has a Sea & Shell Museum on the second floor, and a Historical Museum in the basement.',
+    point3:
+      'The town’s cemetery is a beautiful spot with amazing views. It dates from1856, when U.S. Navy sailor, Gustave Englebrecht was killed in the Battle of Port Gamble and buried there.  He was the first U.S. Navy combat casualty in the Pacific. The cemetery holds 98 graves for residents of all ages. Most passed away in the 19th and early 20th centuries',
+    pointBonus:
+      'Port Gamble has been the setting for books, movies and television programs.',
     go: '',
     eat: '',
     drink: '',
