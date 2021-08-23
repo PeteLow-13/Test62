ed0b61c337ba5be7c355003cb3d0b605a238a867
Pete Low <pedritolow@gmail.com>
2021-08-23 16:09:51 -0700

    working on Go

--- frontend/src/Components/Go.js
+++ frontend/src/Components/Go.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Img } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import TagBar from './TagBar';
 
 const Go = () => {
   const locationDetails = useSelector((state) => state.locationDetails);
   const { location } = locationDetails;
-
   return (
     <>
       <Row className='go' id='go'>
         <Col>
           <h2>how to go {location.name}</h2>
+          <Img src={location.image} />
+          <h3>{location.go1.h3}</h3>
+          <ul>{location.go1.li.map()}</ul>
           {/* <p>{location.knowContent}</p> */}
         </Col>
         <TagBar />
