065149a830122c8e59f6f1a16d702228675028ea
Pete Low <pedritolow@gmail.com>
2021-09-06 12:31:46 -0700

    Drink complonent updated for db

--- frontend/src/Components/Drink.js
+++ frontend/src/Components/Drink.js
@@ -10,51 +10,48 @@ const Drink = () => {
   return (
     <>
       <Row className='drink' id='drink'>
-        <h2>where to drink {location.name}</h2>
+        <h2>Where to Drink {location.name}</h2>
         <Col sm={12} md={4} lg={4} xl={4}>
           <Card>
-            <Card.Img src={location.image} />
+            <Card.Img src={location.drink1Image} />
             <Card.Body>
-              <Card.Title>Scratch kitchen</Card.Title>
+              <Card.Title>{location.drink1name}</Card.Title>
               <Card.Text className='address'>
-                32420 N Rainier Ave ​ Port Gamble, WA 98364
+                {location.drink1Address}
               </Card.Text>
-              <Card.Text className='phone'>(360) 297-7636</Card.Text>
+              <Card.Text className='phone'>{location.drink1Phone}</Card.Text>
               <Card.Text className='description'>
-                Good food, cold beer, nice people, awesome view. Local and
-                organic ingredients, offering compostable to go products too!
+                {location.drink1Description}
               </Card.Text>
             </Card.Body>
           </Card>
         </Col>
         <Col sm={12} md={4} lg={4} xl={4}>
           <Card>
-            <Card.Img src={location.image} />
+            <Card.Img src={location.drink2Image} />
             <Card.Body>
-              <Card.Title>Port Gamble General Store</Card.Title>
+              <Card.Title>{location.drink2name}</Card.Title>
               <Card.Text className='address'>
-                32400 N Rainier Ave ​Port Gamble, WA 98364{' '}
+                {location.drink2Address}
               </Card.Text>
-              <Card.Text className='phone'>(360) 297-7636</Card.Text>
+              <Card.Text className='phone'>{location.drink2Phone}</Card.Text>
               <Card.Text className='description'>
-                A full cafe menu including sandwiches, soups, fresh baked goods,
-                ice cream, snacks, an espresso bar, and retail space with gifts.
+                {location.drink2Description}
               </Card.Text>
             </Card.Body>
           </Card>
         </Col>
         <Col sm={12} md={4} lg={4} xl={4}>
           <Card>
-            <Card.Img src={location.image} />
+            <Card.Img src={location.drink3Image} />
             <Card.Body>
-              <Card.Title>Butcher & Baker</Card.Title>
+              <Card.Title>{location.drink3name}</Card.Title>
               <Card.Text className='address'>
-                4719 NE State Highway 104, Port Gamble WA 98364
+                {location.drink3Address}
               </Card.Text>
-              <Card.Text className='phone'>(360) 297-9500</Card.Text>
+              <Card.Text className='phone'>{location.drink3Phone}</Card.Text>
               <Card.Text className='description'>
-                A locally sourced farmhouse restaurant, butcher shop and bakery
-                that offers small batch provisions and dining.
+                {location.drink3Description}
               </Card.Text>
             </Card.Body>
           </Card>
