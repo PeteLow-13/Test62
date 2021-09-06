c84394f73b98c1a4aa3f9d4ae691e61238d8ab02
Pete Low <pedritolow@gmail.com>
2021-09-05 19:02:41 -0700

    updated go component

--- frontend/src/Components/Go.js
+++ frontend/src/Components/Go.js
@@ -9,12 +9,23 @@ const Go = () => {
   return (
     <>
       <Row className='go' id='go'>
-        <Col>
+        <Col md={12}>
           <h2>how to go {location.name}</h2>
-          <Image src={location.image} />
-          <h3>{location.go1.h3}</h3>
-          <ul>{location.go1.li.map()}</ul>
-          {/* <p>{location.knowContent}</p> */}
+
+          <Image src={location.goImage} alt={location.name} fluid />
+          <h3>{location.go1h3}</h3>
+          <p>{location.go1all}</p>
+          <h3>{location.go2h3}</h3>
+          <p>{location.go2all}</p>
+          <h3>{location.go3h3}</h3>
+          <p>{location.go3all}</p>
+          {/* <ul>
+            <li>
+              <p>{location.go1li1}</p>
+            </li>
+            <li>{location.go1li2}</li>
+            <li>{location.go1li3}</li>
+          </ul> */}
         </Col>
         <TagBar />
       </Row>
