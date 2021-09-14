6a89da52ab796607115366436fbb0a452ac60992
Pete Low <pedritolow@gmail.com>
2021-09-14 15:18:33 -0700

    added content to AboutScreen

--- frontend/src/screens/AboutScreen.js
+++ frontend/src/screens/AboutScreen.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
 import IconBar from '../images/travelicons.jpg';
+import './AboutScreen.css';
 
 const AboutScreen = () => {
   return (
     <>
-      <h1>about us</h1>
-      <div className='second-section'>
+      <h1>About us</h1>
+      <div className='text-section'>
         <p>
           The Point of Going is a travel site, dedicated to sharing unique
           places, hidden treasures, and more in the Pacific Northwest and
@@ -17,7 +18,18 @@ const AboutScreen = () => {
           these places, show you why you might want to visit, show you how you
           can get there, and share things to do when you arrive.
         </p>
-        <p>We also look forward to your feedback and recomendations.</p>
+        <p>We also look forward to your feedback and recommendations.</p>
+        <p>
+          Let us know if you want to advertise, or sponsor a business or
+          location.
+        </p>
+        <p>
+          Also, check out{' '}
+          <a href='http://www.mysteryislandstudio.com' target='blank'>
+            Mystery Island Studio
+          </a>
+          , that created this site.
+        </p>
       </div>
       <Image src={IconBar} alt='icon bar' fluid />
     </>
--- frontend/src/screens/PoulsboScreen.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Row, Col, Image } from 'react-bootstrap';
-
-const PoulsboScreen = () => {
-  return <></>;
-};
-
-export default PoulsboScreen;
